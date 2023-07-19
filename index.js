cd6eac95bd0aa279a941572dae39a51ad6c1426b
David James Dimalanta <david.dimalanta@mail.utoronto.ca>
2023-07-19 15:14:46 -0400

    server message

--- index.js
+++ index.js
@@ -75,3 +75,12 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
 });
 
+// Specify the PORT
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log('Server started successfully!');
+});
+
