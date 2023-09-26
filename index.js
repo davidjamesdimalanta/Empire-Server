d107f494a751b27422d303fd30055f3af3f64ed8
David James Dimalanta <david.dimalanta@mail.utoronto.ca>
2023-09-25 22:45:59 -0400

    payment fixes

--- index.js
+++ index.js
@@ -222,20 +222,25 @@ app.post('/api/v1/auth/google', async (req, res) => {
   };
   
   app.post("/create-payment-intent", async (req, res) => {
-    console.log("Received request for /create-payment-intent");
-    const { items } = req.body;
+    try {
+        console.log("Received request for /create-payment-intent");
+        const { items } = req.body;
+      
+        // Create a PaymentIntent with the order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: calculateOrderAmount(items),
+          currency: "usd",
+          // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+          automatic_payment_methods: {
+            enabled: true,
+          },
+        });
   
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculateOrderAmount(items),
-      currency: "usd",
-      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-  
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  });
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+  }
+  catch (error) {
+    console.error("Error processing payment:", error.message);
+        res.status(500).send({ message: "Internal Server Error" });
+  }});
